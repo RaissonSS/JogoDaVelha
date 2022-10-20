8cbdd864bbb8ca7f0211dba408a8c8ed8f8df343
Mariane pigatto <maripigatto04@gmail.com>
2022-10-19 21:43:28 -0300

    função Machine alterada

---
+++ classes.py
@@ -0,0 +1,26 @@
+from txtFileClass import txtFile as Score
+
+class Player:
+    pass
+    def __init__(self, nome, pontuacao, locaisE):
+        self.nome = nome 
+        self.pontuacao = pontuacao
+        self.locaisE = locaisE 
+
+    #def escolherlugar(self, locau):
+        
+
+class Machine:
+    pass
+    def __init__(self, Machine, Score):
+        self.Machine = Machine
+        self.Score = Score
+        #just to test 
+
+
+
+class Game:
+    pass
+
+
+
