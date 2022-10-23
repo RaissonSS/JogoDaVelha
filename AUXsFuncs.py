00ab568f0a9679e8f0e774f1b1e088eac2416395
Mariane pigatto <maripigatto04@gmail.com>
2022-10-23 16:06:37 -0300

    mudei coisas no arquivo teste.py

--- AUXsFuncs.py
+++ AUXsFuncs.py
@@ -1,6 +1,6 @@
 from os import system
 
-def Name(playerNumber):  # Trata inputs destinados ao nome
+def Name(playerNumber):  # Trata inputs destinados ao nome                          # está funcionando
     finished = False
     while finished != True:
         name = str(input(f"Digite o nome do { playerNumber }° jogador: ")).strip()
@@ -14,7 +14,7 @@ def Name(playerNumber):  # Trata inputs destinados ao nome
     return name.capitalize()
 
 
-def Option(msg, max, min=1):  # Trata inputs destinados a opções
+def Option(msg, max, min=1):  # Trata inputs destinados a opções         # está funcionando
     finished = False
     while finished != True:
         try:
@@ -23,11 +23,11 @@ def Option(msg, max, min=1):  # Trata inputs destinados a opções
             print("\nDigite um número!\n")
         else:
             if x > max or x < min:
-                print("\nNúmero além do limite!\n")
+                print("\nNúmero além do limite!\n")        # não seri numero invalido para este jogo
             else:
                 finished = True
     return x
             
 
-def Refresh():  # Limpa o terminal
+def Refresh():  # Limpa o terminal                      # está funcionando
     system('cls')
